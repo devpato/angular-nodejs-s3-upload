82f55a00996305a33c0275107f0aeb6a1d2205df
Patricio Vargas <pevargas2730@gmail.com>
2019-10-21 12:15:40 -0400

    server clean up

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const app = express();
 const cors = require('cors');
 const upload = require('./middleware/file-upload');
-var app = express();
 
 app.use(bodyParser.json());
-app.use(cors);
+app.use(cors());
 
-app.post('api/v1/upload', upload.array('image', 1), (req, res, next) => {
-  console.log('Image name', req.file);
-  res.send('Image uploaded to S3!');
+app.post('/api/v1/upload', upload.array('image', 1), (req, res) => {
+  res.send({ image: req.file });
 });
-
 app.listen(3000, () => {
   console.log('Server listening on port 3000!');
 });
