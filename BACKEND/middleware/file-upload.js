baf49882ccf1e489926100c93ac533bed0d8d781
Patricio Vargas <pevargas2730@gmail.com>
2019-10-21 12:15:21 -0400

    code cleanup

--- BACKEND/middleware/file-upload.js
+++ BACKEND/middleware/file-upload.js
@@ -1,31 +1,34 @@
 const aws = require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
-const s3 = new aws.S3();
+const dotenv = require('dotenv');
+dotenv.config();
 
 aws.config.update({
-  secretAccessKey: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
-  accessKeyId: 'XXXXXXXXXXXXX',
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  accessKeyId: process.env.ACCESS_KEY_ID,
   region: 'us-east-1'
 });
 
-const fileFilterFunction = (req, file, cb) => {
+const s3 = new aws.S3();
+
+const fileFilter = (req, file, cb) => {
   if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
     cb(null, true);
   } else {
-    cb(new Error('Invalid file type, use JPEG or PNG'), false);
+    cb(new Error('Invalid file type, only JPEG and PNG is allowed!'), false);
   }
 };
 
 const upload = multer({
-  fileFilter: fileFilterFunction,
+  fileFilter: fileFilter,
   storage: multerS3({
     acl: 'public-read',
     s3,
-    bucket: 'porellas',
+    bucket: 's2-tutorial',
     key: function(req, file, cb) {
-      req.file = file;
-      cb(null, file.originalname + Date.now());
+      req.file = Date.now() + file.originalname;
+      cb(null, Date.now() + file.originalname);
     }
   })
 });
