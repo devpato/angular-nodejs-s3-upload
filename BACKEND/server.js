12355442fe1ceaaa553d98c95e85ed32bf5e5db8
Patricio Vargas <pevargas2730@gmail.com>
2019-10-21 23:23:39 -0400

    server js file for tutorial

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -2,14 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
-const upload = require('./middleware/file-upload');
+/* Import your upload middleware */
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post('/api/v1/upload', upload.array('image', 1), (req, res) => {
-  res.send({ image: req.file });
-});
+/* You POST route here */
+
 app.listen(3000, () => {
   console.log('Server listening on port 3000!');
 });
