95d7ade94307aceb6f7feb93afcb3dc332af7d19
Patricio Vargas <pevargas2730@gmail.com>
2019-10-21 23:25:19 -0400

    component code clean up

--- ng-upload/src/app/app.component.ts
+++ ng-upload/src/app/app.component.ts
@@ -1,27 +1,8 @@
 import { Component } from '@angular/core';
-import { ImageUploadService } from './image-upload.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  imageObj: File;
-  imageUrl: string;
-
-  constructor(private imageUploadService: ImageUploadService) {}
-
-  onImagePicked(event: Event): void {
-    const FILE = (event.target as HTMLInputElement).files[0];
-    this.imageObj = FILE;
-  }
-
-  onImageUpload() {
-    const imageForm = new FormData();
-    imageForm.append('image', this.imageObj);
-    this.imageUploadService.imageUpload(imageForm).subscribe(res => {
-      this.imageUrl = res['image'];
-    });
-  }
-}
+export class AppComponent {}
